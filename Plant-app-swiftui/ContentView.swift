439157aecb3aaaeea9ffac002e05cb8259d528de
Charles Roberts <cjroberts2013@gmail.com>
2022-07-17 12:06:43 -0400

    Initial Commit

---
+++ Plant-app-swiftui/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Plant-app-swiftui
+//
+//  Created by Charles Roberts on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plant-app-swiftui/Plant_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_app_swiftuiApp.swift
+//  Plant-app-swiftui
+//
+//  Created by Charles Roberts on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Plant_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
